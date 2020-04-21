69809ead313ee3663a6ae574e579519d358b8ae5
codejedi365 <codejedi365@gmail.com>
2020-04-21 19:48:58 -0400

    DEV/Insight-Vue > Reset folder structure to Vue CLI@^4.0.0

--- src/insight/client/src/views/Home.vue
+++ src/insight/client/src/views/Home.vue
@@ -1,21 +1,18 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js + Flask App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "Home",
   components: {
     HelloWorld
   }
-}
+};
 </script>
-
-<style lang="scss">
-
-</style>
