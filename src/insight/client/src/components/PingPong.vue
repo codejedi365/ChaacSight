9c0ab3a7acfb13b159c2b00bef35c9a53f3d49ca
codejedi365 <codejedi365@gmail.com>
2020-05-02 15:41:40 -0400

    DEV/Insight-Vue > Implement PingPong page to prepare ClientUI for API connection testing
    PingPong is a test page that will send a request with axios to a specified
    URL.  This one is configured to send to localhost:5000/ping to talk to
    the API server.  In the test case, the API server is faked thanks to Sinon's
    fake-server functionality.  Issues were found with the synchronous
    execution so 'flush-promises' module was added to ensure assertions
    were executed after the request returned.

--- src/insight/client/src/components/PingPong.vue
+++ src/insight/client/src/components/PingPong.vue
@@ -1,16 +1,45 @@
 <template>
-  <div>
-    <p>{{ msg }}</p>
-  </div>
+    <div>
+        <div class="button" style="display:inline-block" @click="pingServer">{{ msg }}</div>
+    </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: 'Ping',
-  data() {
-    return {
-      msg: 'Hello!',
-    };
-  },
+    name: "Ping",
+    methods: {
+        pingServer: function() {
+			const path = "http://localhost:5000/ping";
+            axios
+                .get(path)
+                .then(res => {
+                    this.msg = res.data;
+					console.log("promise resolved! ("+this.msg+")")
+                })
+                .catch(error => {
+                    if (process.env.NODE_ENV !== "production") {
+                        console.error("SENT ['"+error.request.method+"','"+error.request.url+"']")
+                        console.error(error.message);
+                    }
+                    if (error.response.status == "408") {       // 408 Request Timeout
+                        // Unable to reach API server (check your internet connection, retrying in?)
+
+                    } else if (error.response.status == "404") {        // 404 Not Found
+                        // API entrypoint invalid
+
+                    } else {
+                        // Unexpected error. Please Try again later.
+
+                    }
+				});
+        }
+    },
+    data() {
+        return {
+            msg: "Ping"
+        };
+    }
 };
 </script>
--- src/insight/client/tests/unit/components/PingPong.spec.js
+++ src/insight/client/tests/unit/components/PingPong.spec.js
@@ -1,30 +1,81 @@
-import { expect } from "chai";
+import { assert } from "chai";
+import sinon from "sinon";
 import { shallowMount } from "@vue/test-utils";
+import flushPromises from "flush-promises";
 import PingPong from "@/components/PingPong.vue";
 
+const apiURL = "http://localhost:5000";
+var sinonServer;
+
 describe("PingPong.vue", () => {
-  // Component Contract
-  it("submits request to API server when button is clicked", () => {
+    const requestPairs = [
+        { [ [apiURL, "ping"].join("/") ]: "Pong" },
+    ]
 
-  });
-  it("stores API response message on success", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(PingPong, {
-      propsData: { msg }
+    beforeEach(() => {
+        sinonServer = sinon.fakeServer.create();
+        sinonServer.respondImmediately = true;
+        // sinonServer.respondWith([apiURL, "ping"].join("/"), "Pong")
+        // requestPairs.forEach(item => {
+        //     var url = Object.keys(item)[0];
+        //     sinonServer.respondWith(url,item[url]);
+        // },this);
     });
-    expect(wrapper.text()).to.include(msg);
-  });
-  it("throws error if API request fails", () => {
+    afterEach(() => {
+        sinonServer.restore();
+        // Restore the default sandbox here
+        sinon.restore();
+    });
 
-  });
-  it("throws error if API request exceeds timeout", () => {
+    // Component Contract
+    it("submits request to API server when button is clicked", async () => {
+        sinonServer.respondWith("GET", [apiURL, "ping"].join("/"), "Pong")
+        assert.strictEqual(sinonServer.requests.length, 0, 'No request yet');
 
-  });
-  // Snapshot
-  it("renders correctly on load", () => {
+        const wrapper = shallowMount(PingPong);
+        wrapper.get("div.button").trigger("click");
+        await flushPromises();
 
-  });
-  it("renders correctly after API response", () => {
+        assert.strictEqual(sinonServer.requests.length, 1, 'Only one request');
+		assert.strictEqual(sinonServer.requests[0].url, [apiURL, "ping"].join("/"), 'correct API URL');
+    });
+    it("stores API response message on success", async () => {
+        const responseMsg = "Pong";
+        sinonServer.respondWith("GET", [apiURL, "ping"].join("/"), responseMsg)
 
-  });
+        const wrapper = shallowMount(PingPong);
+        assert.notEqual(wrapper.vm.msg, responseMsg);
+
+        wrapper.get("div.button").trigger("click");
+        await flushPromises();
+
+        assert.equal(wrapper.vm.msg, responseMsg);
+    });
+    it("throws error if API request fails", async () => {
+        // no server path set up so default is 404 error
+
+        // replace the console.log since this will throw an axios super error
+        // const fakeConsole = sinon.fake();
+        // sinon.replace(console, "error", fakeConsole)
+
+        const wrapper = shallowMount(PingPong);
+        // assert.equal(wrapper.vm.??,0,'No errors yet')
+
+        wrapper.get("div.button").trigger("click");
+        await flushPromises();
+
+        assert.fail("Test 50% Implemented")
+        // assert.equal(wrapper.vm.??, 1, '1 error found');
+    });
+    /*
+    it("throws error if API request exceeds timeout", () => {
+        assert.fail("Test Not Implemented");
+    });
+    // Snapshot
+    it("renders correctly on load", () => {
+        assert.fail("Test Not Implemented");
+    });
+    it("renders correctly after API response", () => {
+        assert.fail("Test Not Implemented");
+    });*/
 });
